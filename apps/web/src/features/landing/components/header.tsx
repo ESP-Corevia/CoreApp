6e5f31a1384309be4762ae77598f0695f0043be3
Tristan <tristan.chardes@gmail.com>
2025-11-13 15:32:21 +0100

    wip 4

--- apps/web/src/features/landing/components/header.tsx
+++ apps/web/src/features/landing/components/header.tsx
@@ -2,26 +2,23 @@ import { Link } from "react-router";
 import { Button } from "@/components/ui/button";
 
 export default function Header({
-  alreadyLogin
+  alreadyLogin,
 }: {
-  alreadyLogin?: boolean
+  alreadyLogin?: boolean;
 }) {
   return (
-    <header className="bg-black text-green-400">
+    <header className="bg-white text-black border-b border-gray-200 shadow-sm">
       <div className="max-w-7xl mx-auto flex items-center justify-between px-6 py-4">
-        {/* Logo / Nom */}
         <Link
           to="/"
-          className="text-xl font-semibold hover:text-green-300 transition-colors"
+          className="text-xl font-semibold hover:text-gray-600 transition-colors decoration-[#19CB1F] underline"
         >
           Corevia
         </Link>
 
-        {/* Bouton Login */}
         <Link to={alreadyLogin ? "/home" : "/login"}>
           <Button
-            variant="outline"
-            className="border-green-400 text-green-400 hover:bg-green-400 hover:text-white transition-colors"
+            className="border-[#19CB1F] cursor-pointer border-2 border-solid bg-white hover:text-gray-600 transition-colors"
           >
             {alreadyLogin ? "Home" : "Login"}
           </Button>
