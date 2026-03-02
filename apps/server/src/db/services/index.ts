76972e3e983d7b7f665bbf5d2275f9cfa5607186
Mattéo <matteo.cogan@epitech.eu>
2026-03-02 15:24:30 +0100

    feat(server): register aiMetrics service in services container [KAN-25]

--- apps/server/src/db/services/index.ts
+++ apps/server/src/db/services/index.ts
@@ -9,6 +9,7 @@ import {
 } from '../repositories';
 
 import { createAppointmentsService } from './appointments.service';
+import { createAiMetricsService } from './aiMetrics.service';
 import { createAvailabilityService } from './availability.service';
 import { createDoctorsService } from './doctors.service';
 import { createMedicationsService } from './medications.service';
@@ -18,6 +19,7 @@ import { createUsersService } from './users.service';
 const medicationsProvider = createMedicationsProvider();
 
 export const usersService = createUsersService(usersRepo);
+export const aiMetricsService = createAiMetricsService();
 export const doctorsService = createDoctorsService(doctorsRepo, usersRepo);
 export const patientsService = createPatientsService(patientsRepo, usersRepo);
 export const availabilityService = createAvailabilityService(availabilityRepo, doctorsRepo);
@@ -25,6 +27,7 @@ export const appointmentsService = createAppointmentsService(appointmentsRepo);
 export const medicationsService = createMedicationsService(medicationsRepo, medicationsProvider);
 export const services = {
   usersService,
+  aiMetricsService,
   doctorsService,
   patientsService,
   availabilityService,
@@ -33,6 +36,7 @@ export const services = {
 };
 export type Services = {
   usersService: ReturnType<typeof createUsersService>;
+  aiMetricsService: ReturnType<typeof createAiMetricsService>;
   doctorsService: ReturnType<typeof createDoctorsService>;
   patientsService: ReturnType<typeof createPatientsService>;
   availabilityService: ReturnType<typeof createAvailabilityService>;
