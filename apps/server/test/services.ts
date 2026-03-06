3adabb30de80b6997041bbc8d14bedd4e50e0f35
Yasser Mekhfi <75505765+Yasser5711@users.noreply.github.com>
2026-03-06 12:21:09 +0100

    Revert "feat: implement availability service and repository for doctor appoin…" (#86)
    This reverts commit a96b7da258ae2540b95aa7016d986cb9f351392b.

--- apps/server/test/services.ts
+++ apps/server/test/services.ts
@@ -1,20 +1,15 @@
 import { mockDeep, type DeepMockProxy } from 'vitest-mock-extended';
 
-import type { createAvailabilityService } from '../src/db/services/availability.service';
 import type { createDoctorsService } from '../src/db/services/doctors.service';
 import type { createUsersService } from '../src/db/services/users.service';
 
 type UsersService = ReturnType<typeof createUsersService>;
 type DoctorsService = ReturnType<typeof createDoctorsService>;
-type AvailabilityService = ReturnType<typeof createAvailabilityService>;
 
 export const mockUsersService: DeepMockProxy<UsersService> = mockDeep<UsersService>();
 export const mockDoctorsService: DeepMockProxy<DoctorsService> = mockDeep<DoctorsService>();
-export const mockAvailabilityService: DeepMockProxy<AvailabilityService> =
-  mockDeep<AvailabilityService>();
 
 export const mockServices = {
   usersService: mockUsersService,
   doctorsService: mockDoctorsService,
-  availabilityService: mockAvailabilityService,
 };
