ed7bbec0d56556f91cfbb7657e01b6d27c31b568
Yasser Mekhfi <75505765+Yasser5711@users.noreply.github.com>
2026-03-06 14:54:34 +0100

    Revert "Revert "feat: add doctors management functionality (#81)" (#88)"
    This reverts commit ec7da4a441b358e4d92bfeb6c7014be255803977.

--- apps/server/src/db/services/index.ts
+++ apps/server/src/db/services/index.ts
@@ -1,11 +1,15 @@
-import { usersRepo } from '../repositories';
+import { doctorsRepo, usersRepo } from '../repositories';
 
+import { createDoctorsService } from './doctors.service';
 import { createUsersService } from './users.service';
 
 export const usersService = createUsersService(usersRepo);
+export const doctorsService = createDoctorsService(doctorsRepo);
 export const services = {
   usersService,
+  doctorsService,
 };
 export type Services = {
   usersService: ReturnType<typeof createUsersService>;
+  doctorsService: ReturnType<typeof createDoctorsService>;
 };
